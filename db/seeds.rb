c5c95c35231eca7f1b7dc57a403a8d2e6784b0c9
ahmed <ahmedfawzy671998@gmailcom>
2019-07-14 05:44:32 +0200

    add login and logout functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Ahmed', password: 'password')
-User.create(username: 'Mohamed', password: 'password')
-User.create(username: 'Omar', password: 'password')
-User.create(username: 'Ali', password: 'password')
-User.create(username: 'Khaled', password: 'password')
 User.create(username: 'ahmed', password: 'password')
+User.create(username: 'mohamed', password: 'password')
+User.create(username: 'omar', password: 'password')
+User.create(username: 'ali', password: 'password')
+User.create(username: 'khaled', password: 'password')
 
 Message.create(body: 'Hey everybody', user_id: User.find(1).id)
 Message.create(body: 'Hello world', user_id: User.find(2).id)
