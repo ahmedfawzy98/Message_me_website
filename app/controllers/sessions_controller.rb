ddb3eb20271719eec357ae7889471bed70b3d5b7
ahmed <ahmedfawzy671998@gmailcom>
2019-07-14 16:32:31 +0200

    create channle chatroom

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new]
   def new; end
 
   def create
@@ -10,6 +11,7 @@ class SessionsController < ApplicationController
     else
       flash.now[:error] = 'There was something wrong with your login information'
       render 'new'
+      # redirect_to login_path
     end
   end
 
@@ -17,5 +19,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'You have successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 end
