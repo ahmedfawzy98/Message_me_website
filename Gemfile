0e90b986b3f39c88fdd4376219e8ccc7976b5298
ahmed <ahmedfawzy671998@gmailcom>
2019-07-14 03:26:12 +0200

    add user and message models

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'bcrypt', '~> 3.1.7'
+gem 'hirb'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Ahmed', password: 'password')
+User.create(username: 'Mohamed', password: 'password')
+User.create(username: 'Omar', password: 'password')
+User.create(username: 'Ali', password: 'password')
+User.create(username: 'Khaled', password: 'password')
+User.create(username: 'ahmed', password: 'password')
+
+Message.create(body: 'Hey everybody', user_id: User.find(1).id)
+Message.create(body: 'Hello world', user_id: User.find(2).id)
+Message.create(body: 'Good morning', user_id: User.find(3).id)
+Message.create(body: "I'm back again", user_id: User.find(4).id)
+Message.create(body: 'What do you want?', user_id: User.find(5).id)
+Message.create(body: '', user_id: User.find(5).id)
