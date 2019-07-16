5089a03652da5f71d26ea05ee0f7f99e4131150b
ahmed <ahmedfawzy671998@gmailcom>
2019-07-16 02:57:48 +0200

    add sign up functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'Welcome to MessageMe App'
+      redirect_to chatroom_path
+    else
+      render 'sessions/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
 
-  # root 'chatroom#index'
-  # get 'login', to: 'sessions#new'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
-  # post 'message', to: 'messages#create'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   mount ActionCable.server, at: '/cable'
 
